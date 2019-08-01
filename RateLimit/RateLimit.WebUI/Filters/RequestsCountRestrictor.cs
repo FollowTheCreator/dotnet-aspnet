bfd68cf8ba71f912357205dd400f79da82a99178
Artyom <Artyomkovalevich.top@gmail.com>
2019-08-01 18:20:28 +0300

    add concurrent requests count restrictor

---
+++ RateLimit/RateLimit.WebUI/Filters/RequestsCountRestrictor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RateLimit.WebUI.Filters
+{
+    public class RequestsCountRestrictorAttribute : Attribute, IAsyncActionFilter, IAsyncResultFilter
+    {
+        private static Semaphore _semaphore;
+
+        public RequestsCountRestrictorAttribute(int count)
+        {
+            _semaphore = new Semaphore(count, count);
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            _semaphore.WaitOne();
+            await next();
+        }
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            _semaphore.Release();
+            await next();
+        }
+    }
+}
