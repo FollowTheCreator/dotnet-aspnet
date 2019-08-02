641a2fefad6ca351539720a1f989f3c5dd1823f9
Artyom <Artyomkovalevich.top@gmail.com>
2019-08-02 10:12:02 +0300

    add dynamic converter

---
+++ RateLimit/RateLimit.WebUI/Utils/Convert.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace RateLimit.WebUI.Utils
+{
+    public class Convert
+    {
+        public static TOut To<TOut>(object input)
+        {
+            return JsonConvert.DeserializeObject<TOut>(JsonConvert.SerializeObject(input));
+        }
+    }
+}
