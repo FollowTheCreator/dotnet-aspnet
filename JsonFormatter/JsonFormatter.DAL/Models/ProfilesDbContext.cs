b6dd933a563cc4f4ba32514711c0aac054c8b3bf
Artyom <Artyomkovalevich.top@gmail.com>
2019-08-05 14:16:35 +0300

    add DAL

---
+++ JsonFormatter/JsonFormatter.DAL/Models/ProfilesDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JsonFormatter.DAL.Models
+{
+    public class ProfilesDbContext : DbContext
+    {
+        public ProfilesDbContext()
+        {
+        }
+
+        public ProfilesDbContext(DbContextOptions<ProfilesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profile>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Email).HasMaxLength(200);
+
+                entity.Property(e => e.Name).HasMaxLength(150);
+            });
+        }
+    }
+}
---
+++ JsonFormatter/JsonFormatter.DAL/Repositories/ProfileRepository.cs
@@ -0,0 +1,29 @@
+﻿using JsonFormatter.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace JsonFormatter.DAL.Repositories
+{
+    public class ProfileRepository : IProfileRepository
+    {
+        private readonly ProfilesDbContext _context;
+
+        public ProfileRepository(ProfilesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Profile> GetByIdAsync(int id)
+        {
+            return await _context
+                .Profiles
+                .FirstOrDefaultAsync(profile => profile.Id == id);
+        }
+
+        public async Task UpdateAsync(Profile profile)
+        {
+            _context.Profiles.Update(profile);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
